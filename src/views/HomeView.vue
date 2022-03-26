1a3f2eb00fb7c00b7c7287364df35ea5e3148576
Mel <meljohnpualon@gmail.com>
2022-03-27 00:36:23 +0800

    fix card container width and breakpoint margins

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main w-full h-screen bg-main-black"> 
     <div class="container">
-      <div class="main-box bg-main-gray-dark h-box-type w-box-type rounded-3xl p-5">
+      <div class="main-box bg-main-gray-dark h-box-type w-box-type rounded-3xl p-5 mx-5">
         <h1>Hey there!</h1>
       </div>
       
