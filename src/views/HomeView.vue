23d064a09f5170d77113fdf23c4508e9b3981189
Mel <meljohnpualon@gmail.com>
2022-05-07 17:58:09 +0800

    change rate arrays into objects

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,8 +7,8 @@
           <h1 class="main-title">How did we do?</h1>
           <p class=" text-gray-400 mb-5">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
           <div class=" flex w-full items-center justify-between mb-5">
-            <div v-for="rate in rates" :key="rate" @click="getRate(rate)">
-              <div class="box-rate">{{ rate }}</div>
+            <div v-for="rate in rates" :key="rate" @click="getRate(rate.rate)">
+              <div class="box-rate">{{ rate.rate }}</div>
             </div>
           </div>
           <button class="btn" @click="displayRate">SUBMIT</button>
@@ -31,7 +31,14 @@ export default {
   name: 'HomeView',
   components: {},
   setup() {
-    const rates = ref([1, 2, 3, 4, 5])
+    const rates = ref([
+      { rate: 1, isActive: false  },
+      { rate: 2, isActive: false  },
+      { rate: 3, isActive: false  },
+      { rate: 4, isActive: false  },
+      { rate: 5, isActive: false  },
+    
+    ])
     let defaultData = ref(true)
     let rateData = ref(null)
 
